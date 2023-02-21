9b0d3fd4a74b96c9f083d9b4a11dc8fed9d32c27
profenna23 <74621322+profenna23@users.noreply.github.com>
2023-02-20 22:00:49 -0800

    hw5 started

--- hw3/routes/orders.js
+++ hw3/routes/orders.js
@@ -20,6 +20,19 @@ router.post('/', function(req, res, next) {
                   }
                 ]
 
+  // Modify this file to access the orders data for the given month from the database and return that data to the client
+  // JSON object you construct and return will have the same structure as before
+  // each time a different month has been selected a different order summary should be displayed based upon what is in the database
+  // server-side javascript should be constructing the SELECT statement and then parsing the results
+
+  // find a way to access what month was clicked
+
+  // THIS IS CODE FOR HW5:
+  //var selectedMonth = ?;
+  //const data = JSON.dbquery(SELECT QUANTITY, TOPPING FROM ORDERS WHERE MONTH=selectedMonth);
+  //create json obj and parse string
+  //const json = JSON.stringify(data);
+
   // create json obj and parse string
   const json = JSON.stringify(array);
 
